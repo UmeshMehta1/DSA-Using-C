afa4a3d68c033d9ca602f3221cc53dc7bf0de183
UmeshMehta1 <ukmehta1245@gmail.com>
2024-06-13 14:55:06 +0545

    changes

--- linkelist/Linklist_Implementation.c
+++ linkelist/Linklist_Implementation.c
@@ -1,18 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <conio.h>
 
 struct node
 {
-
     int data;
     struct node *add;
 };
 
 struct node *start = NULL, *temp, *newNode, *next, *prev;
 
+// Function prototypes
+void create();
+void Display();
+void insert_First();
+void insert_Last();
+void insert_Middle();
+void delete_First();
+void delete_Last();
+void delete_Middle();
+
 int main()
 {
-
     int choice;
 
     do
@@ -51,8 +60,10 @@ int main()
             Display();
             break;
         case 9:
-            Exit();
+            printf("Exiting...");
             break;
+        default:
+            printf("Invalid choice! Please try again.");
         }
     } while (choice != 9);
 
@@ -63,7 +74,7 @@ void create()
 {
     char ch;
     int n;
-    printf("Enter an Element.");
+    printf("Enter an Element: ");
     scanf("%d", &n);
     start = (struct node *)(malloc(sizeof(struct node)));
 
@@ -71,12 +82,12 @@ void create()
     start->add = NULL;
     temp = start;
 
-    printf("Want to Continue..");
+    printf("Want to Continue (y/n): ");
     ch = getche();
 
     while (ch == 'y' || ch == 'Y')
     {
-        printf("Enter Next Element: ");
+        printf("\nEnter Next Element: ");
         scanf("%d", &n);
 
         newNode = (struct node *)(malloc(sizeof(struct node)));
@@ -84,7 +95,8 @@ void create()
         newNode->data = n;
         newNode->add = NULL;
         temp->add = newNode;
-        printf("want to Contiue....");
+        temp = newNode; // Move temp to the new node
+        printf("Want to Continue (y/n): ");
         ch = getche();
     }
 }
@@ -110,43 +122,34 @@ void Display()
 void insert_First()
 {
     int n;
-    if (start == NULL)
-    {
-        printf("list Not Found..");
-    }
-    else
-    {
-        printf("Enter NewElement for insert..");
-        scanf("%d", &n);
-        newNode = (struct node *)(malloc(sizeof(struct node)));
-        newNode->data = n;
-        newNode->add = NULL;
-        newNode->add = start;
-        start = newNode;
-    }
+    printf("Enter New Element to insert: ");
+    scanf("%d", &n);
+    newNode = (struct node *)(malloc(sizeof(struct node)));
+    newNode->data = n;
+    newNode->add = start;
+    start = newNode;
 }
 
 // insert last in LinkedList
 void insert_Last()
 {
     int n;
+    printf("Enter New Element to insert: ");
+    scanf("%d", &n);
+    newNode = (struct node *)(malloc(sizeof(struct node)));
+    newNode->data = n;
+    newNode->add = NULL;
+    temp = start;
+
     if (start == NULL)
     {
-        printf("list Not Found..");
+        start = newNode;
     }
     else
     {
-        printf("Enter NewElement for insert..");
-        scanf("%d", &n);
-        newNode = (struct node *)(malloc(sizeof(struct node)));
-        newNode->data = n;
-        newNode->add = NULL;
-        temp = start;
-
         while (temp->add != NULL)
         {
             temp = temp->add;
-            temp++;
         }
         temp->add = newNode;
     }
@@ -158,26 +161,132 @@ void insert_Middle()
     int n, pos, i = 1;
     if (start == NULL)
     {
-        printf("list Not Found..");
+        printf("List Not Found..");
     }
     else
     {
-        printf("Enter NewElement for insert..");
+        printf("Enter New Element to insert: ");
         scanf("%d", &n);
         newNode = (struct node *)(malloc(sizeof(struct node)));
         newNode->data = n;
         newNode->add = NULL;
 
         printf("Enter the position: ");
-        scanf("\t %d", &pos);
+        scanf("%d", &pos);
 
-        while (i < pos)
+        if (pos == 1)
         {
-            prev = next;
-            next = next->add;
+            newNode->add = start;
+            start = newNode;
+            return;
+        }
+
+        temp = start;
+        while (i < pos - 1 && temp->add != NULL)
+        {
+            temp = temp->add;
             i++;
         }
-        prev->add = newNode;
-        newNode->add = next;
+
+        if (temp->add == NULL)
+        {
+            temp->add = newNode;
+        }
+        else
+        {
+            newNode->add = temp->add;
+            temp->add = newNode;
+        }
+    }
+}
+
+// delete first from linklist;
+void delete_First()
+{
+    if (start == NULL)
+    {
+        printf("List Not Found..");
+    }
+    else
+    {
+        temp = start;
+        start = start->add;
+        printf("Deleted node is %d", temp->data);
+        free(temp);
+    }
+}
+
+// Deleted Last Node from linkelist;
+void delete_Last()
+{
+    if (start == NULL)
+    {
+        printf("List Not Found..");
+    }
+    else
+    {
+        temp = start;
+
+        if (temp->add == NULL)
+        {
+            printf("Deleted Node %d", temp->data);
+            free(temp);
+            start = NULL;
+        }
+        else
+        {
+            while (temp->add != NULL)
+            {
+                prev = temp;
+                temp = temp->add;
+            }
+            prev->add = NULL;
+            printf("Deleted Node %d", temp->data);
+            free(temp);
+        }
+    }
+}
+
+void delete_Middle()
+{
+    int pos, i = 1;
+
+    if (start == NULL)
+    {
+        printf("List Not Found..");
+    }
+    else
+    {
+        printf("Enter the position: ");
+        scanf("%d", &pos);
+
+        if (pos == 1)
+        {
+            temp = start;
+            start = start->add;
+            printf("Deleted node: %d", temp->data);
+            free(temp);
+            return;
+        }
+
+        temp = start;
+        while (i < pos && temp->add != NULL)
+        {
+            prev = temp;
+            temp = temp->add;
+            i++;
+        }
+
+        if (temp->add == NULL)
+        {
+            printf("Position not found.");
+        }
+        else
+        {
+            next = temp->add;
+            prev->add = next;
+            printf("Deleted node: %d", temp->data);
+            free(temp);
+        }
     }
 }
