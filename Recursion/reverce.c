eb5d96e04d68f1e332bef08de4e9abebe5bfc109
UmeshMehta1 <ukmehta1245@gmail.com>
2024-07-03 12:04:38 +0545

    rev

--- Recursion/reverce.c
+++ Recursion/reverce.c
@@ -11,16 +11,31 @@ int main()
     printf("%d", res);
 }
 
+// int rev(int n)
+// {
+//     int r;
+//     int s = 0;
+
+//     while (n > 0)
+//     {
+//         r = n % 10;
+//         s = s * 10 + r;
+//         n = n / 10;
+//     }
+//     return s;
+// }
+
 int rev(int n)
 {
     int r;
-    int s = 0;
+    static int s = 0;
 
-    while (n > 0)
+    if (n == 0)
     {
-        r = n % 10;
-        s = s * 10 + r;
-        n = n / 10;
+        return s;
     }
-    return s;
+
+    r = n % 10;
+    s = s * 10 + r;
+    rev(n / 10);
 }
